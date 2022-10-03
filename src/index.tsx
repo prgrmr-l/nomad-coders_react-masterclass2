12cbc9623a40278c100ee73a92656acd34833078
yougotmyphone <110523886+yougotmyphone@users.noreply.github.com>
2022-10-03 21:25:26 +0900

    styled.d.tsAndDefaultTheme

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { darkTheme, lightTheme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={darkTheme}>
+    <App />
+  </ThemeProvider>
+);
 
 // 혹시 root 때문에 에러나시는 분
 // const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
