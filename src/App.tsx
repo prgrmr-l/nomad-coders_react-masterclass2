7e453b1b4938d65c386bef46beefdbec946c9e9a
yougotmyphone <110523886+yougotmyphone@users.noreply.github.com>
2022-10-03 13:12:44 +0900

    setUpTypescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>Hello</Title>
+    </Wrapper>
   );
 }
 
 export default App;
+
+// 그냥 속편하게
+// npx create-react-app 내 앱 이름 --template typescript
+// npm i --save-dev @types/styled-components
+// npm i styled-components
+// 하세요~!!
+// App, index 내용은 복사하면 되니!!
