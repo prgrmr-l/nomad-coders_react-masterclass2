2d84da752a3d835ffcf0b4d8929fec43a04465a3
yougotmyphone <110523886+yougotmyphone@users.noreply.github.com>
2022-10-03 15:12:37 +0900

    typeingTheProps

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
 import App from "./App";
-
-const darkTheme = {
-  textColor: "whitesmoke",
-  backgroundColor: "#111",
-};
-
-const lightTheme = {
-  textColor: "#111",
-  backgroundColor: "whitesmoke",
-};
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <ThemeProvider theme={darkTheme}>
-    <App />
-  </ThemeProvider>
-);
+root.render(<App />);
 
 // 혹시 root 때문에 에러나시는 분
 // const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
