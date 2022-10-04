5aaf47e79e974ae800ee0d82335ad2f7d5be5209
yougotmyphone <110523886+yougotmyphone@users.noreply.github.com>
2022-10-04 16:27:26 +0900

    useParamsCssResetAddFontColors

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,74 @@
+import { createGlobalStyle } from "styled-components";
 import Router from "./router";
 
+//아래 css style reset
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+//=============위의 @import 포함, 아래 추가=============
+*{
+  box-sizing:border-box;
+}
+body{
+  font-family:'Source Sans Pro', sans-serif;// 구글 폰트 적용
+  background-color:${(props) => props.theme.bgColor};
+  color:${(props) =>
+    props.theme.textColor};//https://flatuicolors.com/palette/gb 적용
+} 
+a{
+  text-decoration:none;
+} // 밑줄같은거 애초에 없앰
+`;
+
 function App() {
-  return <Router />;
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
 }
 
 export default App;
-
-// 그냥 속편하게
-// npx create-react-app 내 앱 이름 --template typescript
-// npm i --save-dev @types/styled-components
-// npm i styled-components
-// 하세요~!!
-// App, index 내용은 복사하면 되니!!
